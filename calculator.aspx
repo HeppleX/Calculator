a51930fe484074ffdae00e41ef5f582a432a056f
hepu Xi <1715441591@qq.com>
2020-06-08 23:04:05 +0800

    添加项目文件。

---
+++ calculator.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="calculator.aspx.cs" Inherits="calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="text-align: center">
+            计算器<br />
+            <asp:TextBox ID="TextBox1" runat="server" ReadOnly="True" Width="110px"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="1" Width="40px" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" Text="2" Width="40px" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" runat="server" Text="3" Width="40px" OnClick="Button3_Click" />
+            <br />
+            <asp:Button ID="Button4" runat="server" Text="4" Width="40px" OnClick="Button4_Click" />
+            <asp:Button ID="Button5" runat="server" Text="5" Width="40px" OnClick="Button5_Click" />
+            <asp:Button ID="Button6" runat="server" Text="6" Width="40px" OnClick="Button6_Click" />
+            <br />
+            <asp:Button ID="Button7" runat="server" Text="7" Width="40px" OnClick="Button7_Click" />
+            <asp:Button ID="Button8" runat="server" Text="8" Width="40px" OnClick="Button8_Click" />
+            <asp:Button ID="Button9" runat="server" Text="9" Width="40px" OnClick="Button9_Click" />
+            <br />
+            <asp:Button ID="Button10" runat="server" Text="0" Width="40px" OnClick="Button10_Click" />
+            <asp:Button ID="Button11" runat="server" Text="+" Width="40px" OnClick="Button11_Click" />
+            <asp:Button ID="Button12" runat="server" OnClick="Button12_Click" Text="-" Width="40px" />
+            <br />
+            <asp:Button ID="Button13" runat="server" Text="✖" Width="40px" OnClick="Button13_Click" />
+            <asp:Button ID="Button14" runat="server" Text="➗" Width="40px" OnClick="Button14_Click" />
+            <asp:Button ID="Button15" runat="server" Text="=" Width="40px" OnClick="Button15_Click" />
+            <br />
+            Copyright by Hepple.X
+        </div>
+    </form>
+</body>
+</html>
---
+++ calculator.aspx.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Threading;
+
+public partial class calculator : System.Web.UI.Page
+{
+    static string num1 = "0", num2 = "0", total = "", sign = "";
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button12_Click(object sender, EventArgs e)
+    {
+        if (sign.Length == 1)
+        {
+            Count();
+            num1 = TextBox1.Text;
+            sign = "-";
+        }
+        else
+        {
+            num1 = TextBox1.Text;
+            TextBox1.Text = "";
+            total = "";
+            sign = "-";
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        total += "1";
+        TextBox1.Text = total;
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        total += "2";
+        TextBox1.Text = total;
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        total += "3";
+        TextBox1.Text = total;
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        total += "4";
+        TextBox1.Text = total;
+    }
+
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        total += "5";
+        TextBox1.Text = total;
+    }
+
+    protected void Button6_Click(object sender, EventArgs e)
+    {
+        total += "6";
+        TextBox1.Text = total;
+    }
+
+    protected void Button7_Click(object sender, EventArgs e)
+    {
+        total += "7";
+        TextBox1.Text = total;
+    }
+
+    protected void Button8_Click(object sender, EventArgs e)
+    {
+        total += "8";
+        TextBox1.Text = total;
+    }
+
+    protected void Button9_Click(object sender, EventArgs e)
+    {
+        total += "9";
+        TextBox1.Text = total;
+    }
+
+    protected void Button10_Click(object sender, EventArgs e)
+    {
+        total += "0";
+        TextBox1.Text = total;
+    }
+
+    protected void Button11_Click(object sender, EventArgs e)
+    {
+        if(sign.Length==1)
+        {
+            Count();
+            num1 = TextBox1.Text;
+            sign = "+";
+        }
+        else
+        {
+            num1 = TextBox1.Text;
+            TextBox1.Text = "";
+            total = "";
+            sign = "+";
+        }
+    }
+
+    protected void Button13_Click(object sender, EventArgs e)
+    {
+        if (sign.Length == 1)
+        {
+            Count();
+            num1 = TextBox1.Text;
+            sign = "*";
+        }
+        else
+        {
+            num1 = TextBox1.Text;
+            TextBox1.Text = "";
+            total = "";
+            sign = "*";
+        }
+    }
+
+    protected void Button14_Click(object sender, EventArgs e)
+    {
+        if (sign.Length == 1)
+        {
+            Count();
+            num1 = TextBox1.Text;
+            sign = "/";
+        }
+        else
+        {
+            num1 = TextBox1.Text;
+            TextBox1.Text = "";
+            total = "";
+            sign = "/";
+        }
+    }
+
+    protected void Button15_Click(object sender, EventArgs e)
+    {
+        Count();
+    }
+
+    protected void Count()
+    {
+        num2 = TextBox1.Text;
+        if(num2=="")
+        {
+            num2 = "0";
+        }
+        switch(sign)
+        {
+            case "+":
+                TextBox1.Text = (double.Parse(num1) + double.Parse(num2)).ToString();
+                num1 = "0";
+                num2 = "0";
+                total = "";
+                sign = "";
+                break;
+            case "-":
+                TextBox1.Text = (double.Parse(num1) - double.Parse(num2)).ToString();
+                num1 = "0";
+                num2 = "0";
+                total = "";
+                sign = "";
+                break;
+            case "*":
+                TextBox1.Text = (double.Parse(num1) * double.Parse(num2)).ToString();
+                num1 = "0";
+                num2 = "0";
+                total = "";
+                sign = "";
+                break;
+            case "/":
+                TextBox1.Text = (double.Parse(num1) / double.Parse(num2)).ToString();
+                num1 = "0";
+                num2 = "0";
+                total = "";
+                sign = "";
+                break;
+        }
+    }
+}
